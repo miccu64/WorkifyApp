f064ac1e58b9ef8b797f9b87e43e88fb38df30fb
Konrad Micek <98konrad98@gmail.com>
2025-04-13 17:07:39 +0200

    Dto tests

---
+++ Backend/Tests/Workify.Api.ExerciseStat.UnitTests/Tests/DTOsValidatorsTests/CreateEditStatDtoValidatorTests/NoteTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+using Workify.Api.ExerciseStat.Models.DTOs.Parameters;
+
+namespace Workify.Api.ExerciseStat.UnitTests.Tests.DTOsValidatorsTests.CreateEditStatDtoValidatorTests
+{
+    public class NoteTests
+    {
+        private readonly CreateEditStatDtoValidator _validator = new();
+        private readonly Fixture _fixture = new();
+
+        public static TheoryData<string> ProperNotes =>
+            [
+                null,
+                "",
+                new string('a', 1),
+                new string('a', 31),
+                new string('a', 111)
+            ];
+
+        [Theory]
+        [MemberData(nameof(ProperNotes))]
+        public void Should_Allow(string note)
+        {
+            // Arrange
+            CreateEditStatDto dto = _fixture.Build<CreateEditStatDto>()
+                .With(dto => dto.Note, note)
+                .Create();
+
+            // Act
+            TestValidationResult<CreateEditStatDto> result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Note);
+        }
+    }
+}
---
+++ Backend/Tests/Workify.Api.ExerciseStat.UnitTests/Tests/DTOsValidatorsTests/CreateEditStatDtoValidatorTests/WeightTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+using Workify.Api.ExerciseStat.Models.DTOs.Parameters;
+
+namespace Workify.Api.ExerciseStat.UnitTests.Tests.DTOsValidatorsTests.CreateEditStatDtoValidatorTests
+{
+    public class WeightTests
+    {
+        private readonly CreateEditStatDtoValidator _validator = new();
+        private readonly Fixture _fixture = new();
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(0.0001)]
+        [InlineData(1)]
+        [InlineData(111.11)]
+        [InlineData(3333)]
+        public void Should_Allow_Greater_Or_Equal_Zero(double weight)
+        {
+            // Arrange
+            CreateEditStatDto dto = _fixture.Build<CreateEditStatDto>()
+                .With(dto => dto.Weight, weight)
+                .Create();
+
+            // Act
+            TestValidationResult<CreateEditStatDto> result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Weight);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-0.01)]
+        [InlineData(-333)]
+        [InlineData(-333.33)]
+        public void Should_Throw_On_Lower_Than_Zero(double weight)
+        {
+            // Arrange
+            CreateEditStatDto dto = _fixture.Build<CreateEditStatDto>()
+                .With(dto => dto.Weight, weight)
+                .Create();
+
+            // Act
+            TestValidationResult<CreateEditStatDto> result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Weight);
+        }
+    }
+}
