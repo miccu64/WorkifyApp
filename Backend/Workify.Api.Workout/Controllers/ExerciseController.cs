e4e3c7b0693aeb950b43b6b363daeeb9e851a659
Konrad Micek <98konrad98@gmail.com>
2025-05-11 12:05:21 +0200

    Exercise controller, Bruno requests

---
+++ Backend/Workify.Api.Workout/Controllers/ExerciseController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Workify.Api.Workout.Models.DTOs;
+using Workify.Api.Workout.Models.DTOs.Parameters;
+using Workify.Api.Workout.Services;
+using Workify.Utils.Extensions;
+
+namespace Workify.Api.Workout.Controllers
+{
+    [ApiController]
+    [Route("api/exercises")]
+    public class ExerciseController(IExerciseService exerciseService) : ControllerBase
+    {
+        private readonly IExerciseService _exerciseService = exerciseService;
+
+        [HttpGet]
+        public async Task<IEnumerable<ExerciseDto>> GetExercises()
+        {
+            int userId = User.GetUserId();
+
+            return await _exerciseService.GetExercises(userId);
+        }
+
+        [HttpPost]
+        public async Task<int> CreateExercise(CreateEditExerciseDto dto)
+        {
+            int userId = User.GetUserId();
+
+            return await _exerciseService.CreateExercise(userId, dto);
+        }
+
+        [HttpPatch("{exerciseId}")]
+        public async Task<int> EditExercise(int exerciseId, CreateEditExerciseDto dto)
+        {
+            int userId = User.GetUserId();
+
+            return await _exerciseService.EditExercise(exerciseId, userId, dto);
+        }
+
+        [HttpDelete("{exerciseId}")]
+        public async Task<int> DeleteExercise(int exerciseId)
+        {
+            int userId = User.GetUserId();
+
+            return await _exerciseService.DeleteExercise(exerciseId, userId);
+        }
+    }
+}
