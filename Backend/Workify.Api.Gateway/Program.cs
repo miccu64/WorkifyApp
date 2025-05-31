474fecc88238947fbf30950c4f38ccee761b1491
Konrad Micek <98konrad98@gmail.com>
2025-05-31 16:35:22 +0200

    CORS policy

--- Backend/Workify.Api.Gateway/Program.cs
+++ Backend/Workify.Api.Gateway/Program.cs
@@ -2,10 +2,18 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+
 builder.Configuration.AddJsonFile("Configuration/ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 await app.UseOcelot();
 
 app.Run();
