0644c29d40ba91b92bf9142cb26cb0e297480fea
Konrad Micek <98konrad98@gmail.com>
2025-08-11 21:36:38 +0200

    Stats caching

--- Frontend/WorkifyApp/src/app/exercises/exercise-preview/stats.resolver.ts
+++ Frontend/WorkifyApp/src/app/exercises/exercise-preview/stats.resolver.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { StatDto } from '../../dtos/stat.dto';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { StatService } from '../../services/stat.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class StatsResolver implements Resolve<StatDto[]> {
-  constructor(private statService: StatService) {}
+  private statService = inject(StatService);
+
   resolve(route: ActivatedRouteSnapshot): Observable<StatDto[]> {
     const exerciseId = Number(route.paramMap.get('exerciseId'));
 
-    return this.statService.getExerciseStats(exerciseId);
+    const cachedStats = this.statService.tryGetCachedStats(exerciseId);
+    if (cachedStats) {
+      return of(cachedStats);
+    } else {
+      return this.statService.getExerciseStats(exerciseId);
+    }
   }
 }
--- Frontend/WorkifyApp/src/app/services/stat.service.ts
+++ Frontend/WorkifyApp/src/app/services/stat.service.ts
@@ -1,15 +1,29 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from '../utils/api.service';
 import { StatDto } from '../dtos/stat.dto';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { CreateEditStatDto } from '../dtos/parameters/create-edit-stat.dto';
 
 @Injectable({ providedIn: 'root' })
 export class StatService {
   private apiService = inject(ApiService);
 
+  private cachedStats: Record<number, StatDto[]> = {};
+
+  tryGetCachedStats(exerciseId: number): StatDto[] | null {
+    return this.cachedStats[exerciseId];
+  }
+
+  cacheStats(exerciseId: number, stats: StatDto[]): void {
+    this.cachedStats[exerciseId] = stats;
+  }
+
   getExerciseStats(exerciseId: number): Observable<StatDto[]> {
-    return this.apiService.get<StatDto[]>(`stats/exercises/${exerciseId}`);
+    return this.apiService.get<StatDto[]>(`stats/exercises/${exerciseId}`).pipe(
+      tap(stats => {
+        this.cacheStats(exerciseId, stats);
+      })
+    );
   }
 
   createStat(exerciseId: number, dto: CreateEditStatDto): Observable<number> {
